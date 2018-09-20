b030892e134addcac89fbb94337b628fa433561b
Saurabh Bansal <bansalsaurabh001@gmail.com>
2018-09-20 21:10:35 +0530

    Changes to be committed:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import {NestedTreeControl} from '@angular/cdk/tree';
+import {Component, Injectable} from '@angular/core';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
+import {BehaviorSubject,Observable, of as observableOf} from 'rxjs';
+
+export class FileNode {
+  children: FileNode[];
+  filename: string;
+  type: any;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tree-app';
+  title = 'Hierarchic Tree';
+
+  nestedTreeControl: NestedTreeControl<FileNode>;
+  nestedDataSource: MatTreeNestedDataSource<FileNode>;
+  dataChange: BehaviorSubject<FileNode[]> = new BehaviorSubject<FileNode[]>([]);
+  constructor() {
+    this.nestedTreeControl = new NestedTreeControl<FileNode>(this._getChildren);
+    this.nestedDataSource = new MatTreeNestedDataSource();
+
+    this.dataChange.subscribe(data => this.nestedDataSource.data = data);
+
+    this.dataChange.next([
+      {
+        filename: "test1",
+        type: "",
+        children: [
+          {
+            filename: "test1.1",
+            type: "",
+            children: [
+              {
+                filename: "test1.1.1",
+                type: "",
+                children: [{
+                  filename: "test1.1.1.1",
+                  type: "",
+                  children: [],
+                },
+                {
+                  filename: "test1.1.1.2",
+                  type: "",
+                  children: [],
+                }
+              ],
+             },
+          ],
+          }
+          ],
+        },
+      {
+        filename: "test2",
+        type: "",
+        children: [
+          {
+            filename: "test2.1",
+            type: "",
+            children: [],
+          }
+        ],
+      },
+      {
+        filename: "test3",
+        type: "",
+        children: [
+          {
+            filename: "test3.1",
+            type: "",
+            children: [],
+          }
+        ],
+      },
+
+    ]);
+  }
+
+
+
+  hasNestedChild = (_: number, nodeData: FileNode) => {return !(nodeData.type); };
+
+  private _getChildren = (node: FileNode) => { return observableOf(node.children); };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTreeModule, MatIconModule, MatButtonModule} from '@angular/material';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
